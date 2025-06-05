65a06036773eee2f6dee5ad65b2155c719c2a15b
Igor Vieira <igor.p.r.vieira@gmail.com>
2025-06-05 00:17:43 -0300

    linting of our setup

--- nvim/lua/plugins/essentials.lua
+++ nvim/lua/plugins/essentials.lua
@@ -45,7 +45,7 @@ return {
   },
   {
     "windwp/nvim-ts-autotag",
-    ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte", "vue", "tsx", "jsx" },
+    ft = { "html", "javascript", "typescript", "javascriptreact", "typescriptreact", "svelte", "vue", "tsx", "jsx", "astro" },
     config = function() require("nvim-ts-autotag").setup() end,
   },
 }
--- nvim/lua/plugins/linting.lua
+++ nvim/lua/plugins/linting.lua
@@ -3,19 +3,51 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local lint = require("lint")
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-    }
+    
+    local function command_exists(cmd)
+      local handle = io.popen("which " .. cmd .. " 2>/dev/null")
+      if handle then
+        local result = handle:read("*a")
+        handle:close()
+        return result ~= ""
+      end
+      return false
+    end
+
+    local available_linters = {}
+    
+    if command_exists("eslint_d") then
+      available_linters.javascript = { "eslint_d" }
+      available_linters.typescript = { "eslint_d" }
+      available_linters.javascriptreact = { "eslint_d" }
+      available_linters.typescriptreact = { "eslint_d" }
+    else
+      local notified = false
+      if not notified then
+        vim.notify("eslint_d not found. Install via :Mason or npm install -g eslint_d", vim.log.levels.WARN)
+        notified = true
+      end
+    end
+
+    lint.linters_by_ft = available_linters
+
+    local function safe_try_lint()
+      local current_ft = vim.bo.filetype
+      if available_linters[current_ft] then
+        local ok, err = pcall(lint.try_lint)
+        if not ok then
+          available_linters[current_ft] = nil
+          lint.linters_by_ft = available_linters
+        end
+      end
+    end
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       group = lint_augroup,
-      callback = function() lint.try_lint() end,
+      callback = safe_try_lint,
     })
 
-    vim.keymap.set("n", "<leader>l", function() lint.try_lint() end, { desc = "Trigger linting" })
+    vim.keymap.set("n", "<leader>l", safe_try_lint, { desc = "Trigger linting" })
   end,
 }
